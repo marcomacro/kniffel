85b2255213830f0f2c51396395d750d79f1aadc9
Marco Haderk <m.haderk@googlemail.com>
2020-12-16 21:33:36 +0100

    extended count_dice_points.php - note: validation not implemented yet

--- count_dice_points.php
+++ count_dice_points.php
@@ -1,4 +1,5 @@
 <?php
+// I. upper block
 
 function count_ones($dices){
     $result = 0;
@@ -44,6 +45,46 @@ function count_sixs($dices){
     $result = 0;
     foreach ($dices as $value){
         if ($value == 6) $result += 6;
+    }
+    return $result;
+}
+
+// II. lower block
+
+// Dreierpasch
+function triple($dices){
+    $result = 0;
+    foreach ($dices as $value) $result += $value;
+    return $result;
+}
+
+// Viererpasch
+function foursome($dices){
+    $result = 0;
+    foreach ($dices as $value) $result += $value;
+    return $result;
+}
+
+function fullhouse($dices){
+    return 25;
+}
+
+function small_street($dices){
+    return 30;
+}
+
+function big_street($dices){
+    return 40;
+}
+
+function yahtzee($dices){
+    return 50;
+}
+
+function chance($dices){
+    $result = 0;
+    foreach ($dices as $value){
+        $result += $value;
     }
     return $result;
 }
--- test_count_dice_points.php
+++ test_count_dice_points.php
@@ -3,9 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Testing: count_dice_points.php</title>
+    <title>Testing page for count_dice_points.php</title>
 </head>
 <body>
+    <h1>Testing page for count_dice_points.php</h1>
     <form action="test_count_dice_points.php" method="POST">
 
         Würfel1:
@@ -17,7 +18,7 @@
         Würfel4:
         <input type="number" name="dice4" value=""><br>
         Würfel5:
-        <input type="number" name="dice5" value=""><br>
+        <input type="number" name="dice5" value="">
         
         <input type="submit" name="test" value="Test">
 
@@ -30,7 +31,7 @@
             include "count_dice_points.php";
 
             echo "<hr>";
-            echo "<h3>Eingegebene Testdaten</h3>";
+            echo "<h2>Eingegebene Testdaten</h2>";
             echo "W1: ".$dices[0]."<br>";
             echo "W2: ".$dices[1]."<br>";
             echo "W3: ".$dices[2]."<br>";
@@ -38,6 +39,7 @@
             echo "W5: ".$dices[4]."<br>";
 
             echo "<hr>";
+            echo "<h2>I. Upper Block</h2>";
             echo "<h3>Teste \"Eins'er zählen\" - function count_ones(..){..}</h3>";
             echo count_ones($dices);
 
@@ -60,6 +62,35 @@
             echo "<hr>";
             echo "<h3>Teste \"Sechs'er zählen\" - function count_sixs(..){..}</h3>";
             echo count_sixs($dices);
+
+            echo "<hr>";
+            echo "<h2>II. Lower Block</h2>";
+            echo "<h3>Teste \"Dreierpasch zählen\" - function triple(..){..}</h3>";
+            echo triple($dices);
+
+            echo "<hr>";
+            echo "<h3>Teste \"Viererpasch zählen\" - function foursome(..){..}</h3>";
+            echo foursome($dices);
+
+            echo "<hr>";
+            echo "<h3>Teste \"Full House zählen\" - function fullhouse(..){..}</h3>";
+            echo fullhouse($dices);
+
+            echo "<hr>";
+            echo "<h3>Teste \"Kleine Straße zählen\" - function small_street(..){..}</h3>";
+            echo small_street($dices);
+
+            echo "<hr>";
+            echo "<h3>Teste \"Große Straße zählen\" - function big_street(..){..}</h3>";
+            echo big_street($dices);
+
+            echo "<hr>";
+            echo "<h3>Teste \"Kniffel zählen\" - function yahtzee(..){..}</h3>";
+            echo yahtzee($dices);
+
+            echo "<hr>";
+            echo "<h3>Teste \"Chance zählen\" - function chance(..){..}</h3>";
+            echo chance($dices);
 
             echo "<hr>";
         }
